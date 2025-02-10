67d2f7018437dc7a3d386964558e4fd8ac66b146
Juan Pablo Zuluaga Mesa <juanpablozuluagamesa@gmail.com>
2025-02-10 17:04:40 -0500

    Update analisis_clientes.py

--- analisis_clientes.py
+++ analisis_clientes.py
@@ -147,7 +147,7 @@ def mapa_clientes(df, filtro=None):
     ruta_mapa = "https://naturalearth.s3.amazonaws.com/50m_cultural\
 /ne_50m_admin_0_countries.zip"
     mundo_dataframe = gpd.read_file(ruta_mapa)
-    mundo_dataframe = mundo_dataframe[mundo_dataframe['CONTINENT'] == continente]
+    mundo_dataframe = mundo_dataframe[mundo_dataframe['CONTINENT'] == 'South America']
 
     fig, ax = plt.subplots()
     mundo_dataframe.plot(ax=ax, color='white', edgecolor='black')
